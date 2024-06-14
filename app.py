8ed2343e6364b51bc064216f04dd31006feab3af
Ritik Singh <ritikpratapsingh29@gmail.com>
2024-06-14 13:54:03 +0530

    cicd first

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_ec2 as ec2,
-    App
+    App,
+    Environment
     # aws_sqs as sqs,
 )
 
@@ -13,8 +14,9 @@ from cicdpipeline.cicdpipeline_stack import CicdpipelineStack
 
 
 app = App()
+env = Environment(account='851725621998', region='us-east-1')
 CicdpipelineStack(app, "CicdpipelineStack",
-   env='851725621998',region='us-east-1'
+   env=env
     )
 
 app.synth()
