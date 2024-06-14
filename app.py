fc0c9c8374c802787b0eea6045438d392e6a988f
Ritik Singh <ritikpratapsingh29@gmail.com>
2024-06-14 13:52:01 +0530

    cicd first

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from aws_cdk import (
 from cicdpipeline.cicdpipeline_stack import CicdpipelineStack
 
 
-app = cdk.App()
+app = App()
 CicdpipelineStack(app, "CicdpipelineStack",
    env='851725621998',region='us-east-1'
     )
